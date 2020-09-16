485f1cf27983f818e2fa9578cb4e9cab50e8a4f8
ssjshengjie <13127202636@163.com>
2020-09-16 16:46:06 +0800

    创建viteVue3项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang="scss">
+  *{
+    padding: 0px;
+    margin: 0px;
+  }
+</style>
