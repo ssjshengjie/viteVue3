3f72a12e0e88d10daf9775889fa9fed8e448c09b
ssjshengjie <13127202636@163.com>
2020-09-19 16:25:43 +0800

    style(Blog-12): 兼容移动端

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,51 @@
   <div class="container">
     <side class="components-layout-size"></side>
     <projects class="components-layout-projects"></projects>
+    <!--  <mousebackground></mousebackground> -->
   </div>
 </template>
 <script>
 import side from './components/AppSideBar.vue';
 import projects from './components/AppProjects.vue';
+import mousebackground from './components/MouseBackground.vue'
 export default {
-  components: { side, projects },
-};
+  components: { side, projects, mousebackground },
+}
 </script>
 <style lang="scss">
-.container {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  border: 0;
-  padding: 0;
-  .components-layout-size {
-    flex-basis: 200px;
+@media screen and (min-width: 540px) {
+  .container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    border: 0;
+    padding: 0;
+    .components-layout-size {
+      width: 200px;
+    }
+    .components-layout-projects {
+      width: calc(100% - 200px);
+      flex: 1;
+    }
   }
-  .components-layout-projects {
-    width: calc(100% - 200px);
-    flex: 1;
+}
+@media screen and (max-width: 540px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+    border: 0;
+    padding: 0;
+    .components-layout-size {
+      flex: 1;
+      width: 100%;
+      height: 100px;
+    }
+    .components-layout-projects {
+      height: calc(100vh - 100px);
+      flex: 1;
+    }
   }
 }
 </style>
--- src/components/AppSideBar.vue
+++ src/components/AppSideBar.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="component-size" v-model="collapsed">
+  <div class="component-size" :class="collapsed ? 'component-size-active' : ''">
     <div class="head-portrait" :class="collapsed ? 'head-portrait-active' : ''"></div>
-    <a-menu theme="dark" v-model:selectedKeys="selectedKeys" mode="inline">
+    <a-menu theme="dark" v-model:selectedKeys="selectedKeys" mode="inline" class="phone-item">
       <a-menu-item v-for="item in Dynamicmenu" :key="item.key" class="menu-item">
         <i class="icon-item" :class="item.icon" :style="collapsed ? 'position: absolute; left: 33%;' : ''"></i>
         <span v-if="!collapsed" class="item-name">{{ item.name }}</span>
       </a-menu-item>
     </a-menu>
-    <div style="border-top: 1px solid #7c7c7c; margin-top: 30px"></div>
+    <div class="component-midline"></div>
     <div class="link-menu" :class="collapsed ? 'link-menu-active' : ''">
       <div class="link-github" :class="collapsed ? 'link-icon-active' : ''" @click="linkOpen('github')"></div>
       <div class="link-cnblogs" :class="collapsed ? 'link-icon-active' : ''" @click="linkOpen('cnblogs')"></div>
       <div class="link-csdn" :class="collapsed ? 'link-icon-active' : ''" @click="linkOpen('csdn')"></div>
     </div>
-    <menu-unfold-outlined v-if="collapsed" class="trigger" @click="() => (collapsed = !collapsed)" />
-    <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
+    <div v-if="collapsed" class="glyphicon glyphicon-hand-right" style="cursor: pointer;" @click="() => (collapsed = !collapsed)" />
+    <div v-else class="glyphicon glyphicon-hand-left" style="cursor: pointer;" @click="() => (collapsed = !collapsed)" />
   </div>
 </template>
 
@@ -71,93 +71,137 @@ export default {
 </script>
 
 <style lang="scss">
-.component-size {
-  width: 100%;
-  height: 100vh;
-  background: black;
-  .head-portrait {
-    height: 150px;
-    width: 150px;
-    border-radius: 50%;
-    background: url("../assets/image/taibai.jpg");
-    background-size: 100% 100%;
-    margin: 16px;
-    margin-left: 25px;
-    margin-bottom: 100px;
-  }
-  .head-portrait-active {
-    height: 50px;
-    width: 50px;
-    position: relative;
-    top: 50px;
-    left: -10px;
-  }
-  .menu-item {
-    display: flex;
-    .icon-item,
-    .item-name {
-      flex: 1;
-      text-align: center;
-      align-items: center;
+@media screen and (min-width: 540px) {
+  .component-size {
+    width: 100%;
+    height: 100vh;
+    background: rgba(0, 0, 0, 1);
+    .ant-menu {
+      background: rgba(0, 0, 0, 1);
     }
-    .icon-item {
-      font-size: 28px;
-      line-height: 1.4 !important;
-    }
-    .item-name {
-      font-size: 18px;
-    }
-  }
-  .link-menu {
-    display: flex;
-    flex-direction: column;
-    margin-left: 25%;
-    width: 100px;
-    height: 350px;
-    margin-top: 30px;
-    .link-github,
-    .link-cnblogs,
-    .link-csdn {
-      width: 100%;
-      height: 100px;
-      border-radius: 30%;
-      margin-bottom: 10px;
-      cursor: pointer;
-      box-shadow: 10px 10px 5px #888888;
-    }
-    .link-github {
-      background: url("../assets/image/github.jpg");
+    .head-portrait {
+      height: 150px;
+      width: 150px;
+      border-radius: 50%;
+      background: url("../assets/image/taibai.jpg");
       background-size: 100% 100%;
+      margin: 16px;
+      margin-left: 25px;
+      margin-bottom: 100px;
     }
-    .link-cnblogs {
-      background: url("../assets/image/cnblogs.jpg");
-      background-size: 100% 100%;
-    }
-    .link-csdn {
-      background: url("../assets/image/csdn.jpg");
-      background-size: 100% 100%;
-    }
-    .link-icon-active {
-      width: 50px;
+    .head-portrait-active {
       height: 50px;
+      width: 50px;
+      position: relative;
+      top: 50px;
+      left: -10px;
+    }
+    .menu-item {
+      display: flex;
+      .icon-item,
+      .item-name {
+        flex: 1;
+        text-align: center;
+        align-items: center;
+      }
+      .icon-item {
+        font-size: 28px;
+        line-height: 1.4 !important;
+      }
+      .item-name {
+        font-size: 18px;
+      }
+    }
+    .menu-item:hover {
+      color: skyblue !important;
+    }
+    .link-menu {
+      display: flex;
+      flex-direction: column;
+      margin-left: 25%;
+      width: 100px;
+      height: 350px;
+      margin-top: 30px;
+      .link-github,
+      .link-cnblogs,
+      .link-csdn {
+        width: 100%;
+        height: 100px;
+        border-radius: 30%;
+        margin-bottom: 10px;
+        cursor: pointer;
+        box-shadow: 10px 10px 5px #888888;
+      }
+      .link-github {
+        background: url("../assets/image/github.jpg");
+        background-size: 100% 100%;
+      }
+      .link-cnblogs {
+        background: url("../assets/image/cnblogs.jpg");
+        background-size: 100% 100%;
+      }
+      .link-csdn {
+        background: url("../assets/image/csdn.jpg");
+        background-size: 100% 100%;
+      }
+      .link-icon-active {
+        width: 50px;
+        height: 50px;
+      }
+    }
+    .link-menu-active {
+      margin-left: 16%;
+    }
+    .component-midline {
+      border-top: 1px solid #7c7c7c;
+      margin-top: 30px;
+    }
+    .head-portrait:hover {
+      animation: head-portrait 0.8s forwards;
+      @keyframes head-portrait {
+        0% {
+          transform: rotate(0);
+          animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+        }
+        50% {
+          transform: rotate(180deg);
+          animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
+    .glyphicon-hand-left {
+      font-size: 50px;
+    }
+    .glyphicon-hand-right {
+      font-size: 40px;
+    }
+    .glyphicon-hand-left:hover,
+    .glyphicon-hand-right:hover {
+      color: skyblue;
     }
   }
-  .link-menu-active {
-    margin-left: 16%;
+  .component-size-active {
+    width: 80px !important;
   }
-  .head-portrait:hover {
-    animation: head-portrait 0.8s forwards;
-    @keyframes head-portrait {
-      0% {
-        transform: rotate(0);
-        animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
-      }
-      50% {
-        transform: rotate(180deg);
-        animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
-      }
-      100% {
-        transform: rotate(360deg);
+  .component-size-phone {
+    display: none !important;
+  }
+}
+@media screen and (max-width: 540px) {
+  .component-size {
+    .head-portrait,
+    .component-midline,
+    .glyphicon-hand-right,
+    .glyphicon-hand-left {
+      display: none;
+    }
+    .phone-item {
+      display: flex;
+      .icon-item {
+        display: none;
       }
     }
   }
