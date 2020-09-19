5d098fa5d8d8b7e9538ed07cb6a698d24e6279d9
ssjshengjie <13127202636@163.com>
2020-09-19 17:18:29 +0800

    styel(Blog-13): 增加媒体查询自适应,适配ipad

--- src/components/AppSideBar.vue
+++ src/components/AppSideBar.vue
@@ -71,7 +71,8 @@ export default {
 </script>
 
 <style lang="scss">
-@media screen and (min-width: 540px) {
+/* pc */
+@media screen and (min-width: 768px) {
   .component-size {
     width: 100%;
     height: 100vh;
@@ -190,6 +191,110 @@ export default {
     display: none !important;
   }
 }
+/* ipad */
+@media screen and (min-width: 540px) and (max-width: 768px) {
+  .component-size {
+    width: 80px !important;
+    height: 100vh;
+    background: rgba(0, 0, 0, 1);
+    .ant-menu {
+      background: rgba(0, 0, 0, 1);
+    }
+    .head-portrait {
+      position: relative;
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+      top: 50px;
+      left: -10px;
+      background: url("../assets/image/taibai.jpg");
+      background-size: 100% 100%;
+      margin: 16px;
+      margin-left: 25px;
+      margin-bottom: 100px;
+    }
+    .menu-item {
+      display: flex;
+      text-align: center;
+      .icon-item,
+      .item-name {
+        flex: 1;
+        padding: 0px;
+        font-size: 18px;
+        align-items: center;
+        text-align: center;
+        justify-content: center;
+        position: relative;
+      }
+      .icon-item {
+        left: -10px;
+        line-height: 35px !important;
+      }
+      .item-name {
+        left: -2px;
+      }
+    }
+    .menu-item:hover {
+      color: skyblue !important;
+    }
+    .link-menu {
+      display: flex;
+      flex-direction: column;
+      margin-left: 25%;
+      width: 100px;
+      height: 350px;
+      margin-top: 30px;
+      margin-left: 16%;
+      .link-github,
+      .link-cnblogs,
+      .link-csdn {
+        width: 50px;
+        height: 50px;
+        border-radius: 30%;
+        margin-bottom: 10px;
+        cursor: pointer;
+        box-shadow: 10px 10px 5px #888888;
+      }
+      .link-github {
+        background: url("../assets/image/github.jpg");
+        background-size: 100% 100%;
+      }
+      .link-cnblogs {
+        background: url("../assets/image/cnblogs.jpg");
+        background-size: 100% 100%;
+      }
+      .link-csdn {
+        background: url("../assets/image/csdn.jpg");
+        background-size: 100% 100%;
+      }
+    }
+    .component-midline {
+      border-top: 1px solid #7c7c7c;
+      margin-top: 30px;
+    }
+    .head-portrait:hover {
+      animation: head-portrait 0.8s forwards;
+      @keyframes head-portrait {
+        0% {
+          transform: rotate(0);
+          animation-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
+        }
+        50% {
+          transform: rotate(180deg);
+          animation-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    }
+    .glyphicon-hand-left,
+    .glyphicon-hand-right {
+      display: none;
+    }
+  }
+}
+/* phone */
 @media screen and (max-width: 540px) {
   .component-size {
     .head-portrait,
@@ -200,8 +305,17 @@ export default {
     }
     .phone-item {
       display: flex;
+      background: #303a50;
+      .ant-menu-item-selected {
+        border-radius: 5px;
+        background-color: tan !important;
+      }
       .icon-item {
-        display: none;
+        color: snow;
+        margin-right: 10px;
+      }
+      .item-name {
+        color: snow;
       }
     }
   }
